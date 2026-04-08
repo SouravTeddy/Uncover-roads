95b4922576602534ff47028f5b47440ca81cb557
Sourav <sourav.bis93@gmail.com>
2026-04-08 18:37:16 +0530

    feat: add MapLibreMap and MapLibreMarkers components with OpenFreeMap tiles

--- frontend/src/modules/map/index.ts
+++ frontend/src/modules/map/index.ts
@@ -1,2 +1,4 @@
 export { MapScreen } from './MapScreen';
 export { TripSheet } from './TripSheet';
+export { MapLibreMap } from './MapLibreMap';
+export { MapLibreMarkers } from './MapLibreMarkers';
