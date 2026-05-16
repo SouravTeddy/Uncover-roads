48ba2a53e8cce2ddab0d7de3288e25510ef7479d
Sourav <sourav.bis93@gmail.com>
2026-05-16 10:18:17 +0530

    feat(persona): export resolvePersonaKey and PersonaDefinition types from module index

--- frontend/src/modules/persona/index.ts
+++ frontend/src/modules/persona/index.ts
@@ -1,2 +1,5 @@
 export { PersonaScreen } from './PersonaScreen';
 export { PersonaModal } from './PersonaModal';
+export { resolvePersonaKey, legacyArchetypeToPersonaKey } from './persona-resolver';
+export type { PersonaKey, PersonaDefinition } from './types';
+export { PERSONA_DEFINITIONS, ARCHETYPE_COLORS } from './types';
